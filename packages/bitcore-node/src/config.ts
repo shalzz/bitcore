835b6bbd36668ccf614caf936378a8c98b0a0a44
Shaleen Jain <shaleen@jain.sh>
2019-04-26 05:39:41 +0000

    add env var to set worker threads

--- packages/bitcore-node/src/config.ts
+++ packages/bitcore-node/src/config.ts
@@ -59,7 +59,7 @@ const Config = function (): ConfigType {
     dbPort: process.env.DB_PORT || '27017',
     dbUser: process.env.DB_USER || '',
     dbPass: process.env.DB_PASS || '',
-    numWorkers: cpus().length,
+    numWorkers: Number(process.env.WORKERS) || cpus().length,
     chains: {},
     services: {
       api: {
--- packages/bitcore-node/src/logger.ts
+++ packages/bitcore-node/src/logger.ts
@@ -1,7 +1,7 @@
 import * as winston from 'winston';
 import parseArgv from './utils/parseArgv';
 let args = parseArgv([], ['DEBUG']);
-const logLevel = args.DEBUG ? 'debug' : 'info';
+const logLevel = args.DEBUG ? 'debug' : 'verbose';
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
